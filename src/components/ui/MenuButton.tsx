2063ca3941cfe098235306f615738ea2fd0ed0ef
Rafael Almeida <almeidarp.dev@gmail.com>
2025-02-01 18:14:10 -0300

    feat: add shadcn avatar and sheet components, and create MenuButton and ProfileButton components

---
+++ src/components/ui/MenuButton.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import { routes } from "@/components/layout/Header";
+import Link from "next/link";
+
+const MenuButton = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className=" w-6 h-6 sm:hidden" />
+      </SheetTrigger>
+      <SheetContent side="left" className="w-[300px]">
+        <nav className="flex flex-col gap-4">
+          {routes.map((route) => (
+            <Link
+              key={route.label}
+              href={route.href}
+              className="block px-2 py-1 text-lg"
+            >
+              {route.label}
+            </Link>
+          ))}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MenuButton;
