7078a2d2f08b959bb34a5229d3db99ec341a449c
Rafael Almeida <almeidarp.dev@gmail.com>
2025-02-01 18:13:29 -0300

    feat: add layout spacing and create container component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/themeProvider";
+import Header from "@/components/layout/Header";
 
 export const metadata: Metadata = {
   title: "TaskFlow App",
   description: "Gerencie suas notas e tarefas de forma simples e eficiente.",
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -14,13 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased`}>
+      <body
+        className={
+          inter.className +
+          `min-h-screen pt-[80px] sm:pt-[90px] md:pt-[100px] antialiased`
+        }
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+          <Header />
           {children}
         </ThemeProvider>
       </body>
